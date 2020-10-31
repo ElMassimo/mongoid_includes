02e595287aedd99bcba2973e1203daef72e4c742
Maximo Mussini <maximomussini@gmail.com>
2020-10-30 22:53:43 -0300

    Automate test coverage reports and update Travis configuration

--- Gemfile
+++ Gemfile
@@ -9,4 +9,5 @@ gem 'mongoid', '~> 7.1.0'
 
 group :test do
   gem 'rspec-given', '~> 3.5'
+  gem 'simplecov', '~> 0.17.0', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-end
+require 'simplecov'
+SimpleCov.start
 
 require 'rspec/given'
 require 'pry'
